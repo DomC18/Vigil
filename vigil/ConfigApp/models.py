6d39b26a55f6bb5c82be84853f29c3c486d80264
Dom Catania <24DCatania@student.nchstx.org>
2024-07-26 13:01:17 -0500

    start of models

--- vigil/ConfigApp/models.py
+++ vigil/ConfigApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Subsystem(models.Model):
+    name = models.CharField(name="Subsystem Name", max_length=50)
+    image = models.ImageField(name="Subsystem Image", blank=True, null=True)
+
+    def __str__(self):
+        return super().__str__()
+
+class Configuration(models.Model):
+    name = models.CharField(name="Robot Name", default="NewRobot", max_length=50, blank=False)
+    image = models.ImageField(name="Robot Image", blank=True, null=True)
+    year = models.IntegerField(name="FRC Year", default=datetime.now().year, blank=True)
+    team_number = models.IntegerField(name="Team Number", default=0, blank=True)
+    drivetrain_type = models.CharField(name="DriveTrain Type", default="Tank Drive", max_length=50, blank=True)
+    subsystems = models.ManyToManyField(Subsystem, blank=True)
+
+    def __str__(self):
+        return super().__str__()
--- vigil/ConfigApp/urls.py
+++ vigil/ConfigApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("config/", views.config, name="config")
+    path("config/", views.config, name="config"),
+    path("newconfig/", views.newconfig, name="newconfig"),
 ]
