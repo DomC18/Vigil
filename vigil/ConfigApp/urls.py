5b8c97237ad0beeefdc384ede574717790f10d08
Dom Catania <24DCatania@student.nchstx.org>
2024-07-31 17:50:28 -0500

    full configapp functionality

--- vigil/ConfigApp/urls.py
+++ vigil/ConfigApp/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     path("newrobotconfig/", views.newrobotconfig, name="newrobotconfig"),
     path("newsubsystemconfig/", views.newsubsystemconfig, name="newsubsystemconfig"),
     path("newsubsystem/", views.newsubsystem, name="newsubsystem"),
+    path("deleterobotconfig/<robotconfig_id>", views.deleterobotconfig, name="deleterobotconfig"),
+    path("deletesubsystemconfig/<subconfig_id>", views.deletesubsystemconfig, name="deletesubsystemconfig"),
+    path("deletesubsystem/<sub_id>", views.deletesubsystem, name="deletesubsystem"),
+    path("editrobotconfig/<robotconfig_id>", views.editrobotconfig, name="editrobotconfig"),
+    path("editsubsystemconfig/<subconfig_id>", views.editsubsystemconfig, name="editsubsystemconfig"),
+    path("editsubsystem/<sub_id>", views.editsubsystem, name="editsubsystem"),
+    path("activaterobotconfig/<robotconfig_id>", views.activaterobotconfig, name="activaterobotconfig"),
+    path("activatesubsystemconfig/<subconfig_id>", views.activatesubsystemconfig, name="activatesubsystemconfig")
 ]
--- vigil/ConfigApp/views.py
+++ vigil/ConfigApp/views.py
@@ -15,7 +15,7 @@ def config(request:WSGIRequest):
     configcontent = robotconfigs
     display_curr_config = True if (robotconfigs and subsystemconfigs and not currconfigs) else False
     display_sub_config = True if (subsystems) else False
-    return render(request, 'config.html', {'content': content, 'configcontent': configcontent, 'display_curr_config': display_curr_config, 'display_sub_config': display_sub_config})
+    return render(request, 'config.html', {'content': content, 'configcontent': configcontent, 'display_curr_config': display_curr_config, 'display_sub_config': display_sub_config, 'link_index': 0})
 
 def subsystemconfigs(request:WSGIRequest):
     currconfigs = get_all_with_username(CurrentConfiguration)
@@ -27,7 +27,7 @@ def subsystemconfigs(request:WSGIRequest):
     configcontent = subsystemconfigs
     display_curr_config = True if (robotconfigs and subsystemconfigs and not currconfigs) else False
     display_sub_config = True if (subsystems) else False
-    return render(request, 'config.html', {'content': content, 'configcontent': configcontent, 'display_curr_config': display_curr_config, 'display_sub_config': display_sub_config})
+    return render(request, 'config.html', {'content': content, 'configcontent': configcontent, 'display_curr_config': display_curr_config, 'display_sub_config': display_sub_config, 'link_index': 1})
 
 def subsystems(request:WSGIRequest):
     currconfigs = get_all_with_username(CurrentConfiguration)
@@ -39,7 +39,63 @@ def subsystems(request:WSGIRequest):
     configcontent = subsystems
     display_curr_config = True if (robotconfigs and subsystemconfigs and not currconfigs) else False
     display_sub_config = True if (subsystems) else False
-    return render(request, 'config.html', {'content': content, 'configcontent': configcontent, 'display_curr_config': display_curr_config, 'display_sub_config': display_sub_config})
+    return render(request, 'config.html', {'content': content, 'configcontent': configcontent, 'display_curr_config': display_curr_config, 'display_sub_config': display_sub_config, 'link_index': 2})
+
+def deleterobotconfig(request, robotconfig_id):
+    robotconfig = RobotConfiguration.objects.get(pk=robotconfig_id)
+    robotconfig.delete()
+    return redirect('config')
+
+def deletesubsystemconfig(request, subconfig_id):
+    subsystemconfig = SubsystemConfiguration.objects.get(pk=subconfig_id)
+    subsystemconfig.delete()
+    return redirect('subsystemconfigs')
+
+def deletesubsystem(request, sub_id):
+    subsystem = Subsystem.objects.get(pk=sub_id)
+    subsystem.delete()
+    return redirect('subsystems')
+
+def editrobotconfig(request, robotconfig_id):
+    robotconfig = RobotConfiguration.objects.get(pk=robotconfig_id)
+    form = RobotConfigForm(request.POST or None, instance=robotconfig)
+    if form.is_valid():
+        form.save()
+        return redirect('robotconfigs')
+    return render(request, "updaterobotconfig.html", {'form': form, 'name': gv.current_user, 'instance': robotconfig})
+
+def editsubsystemconfig(request, subconfig_id):
+    subconfig = SubsystemConfiguration.objects.get(pk=subconfig_id)
+    subsystems = get_all_with_username(Subsystem)
+    form = SubsystemConfigForm(request.POST or None, instance=subconfig)
+    if form.is_valid():
+        form.save()
+        return redirect('subsystemconfigs')
+    return render(request, "updatesubsystemconfig.html", {'form': form, 'name': gv.current_user, 'instance': subconfig, 'subsystems': subsystems})
+
+def editsubsystem(request, sub_id):
+    subsystem = Subsystem.objects.get(pk=sub_id)
+    form = SubsystemForm(request.POST or None, instance=subsystem)
+    if form.is_valid():
+        form.save()
+        return redirect('subsystems')
+    return render(request, "updatesubsystem.html", {'form': form, 'name': gv.current_user, 'instance': subsystem})
+
+def activaterobotconfig(request, robotconfig_id):
+    currconfig = get_all_with_username(CurrentConfiguration)
+    if currconfig:
+        robotconfig = RobotConfiguration.objects.get(pk=robotconfig_id)
+        currconfig[0].RobotConfig = robotconfig
+        currconfig[0].save()
+    return redirect('robotconfigs')
+
+def activatesubsystemconfig(request, subconfig_id):
+    currconfig = get_all_with_username(CurrentConfiguration)
+    if currconfig:
+        subconfig = SubsystemConfiguration.objects.get(pk=subconfig_id)
+        currconfig[0].SubsystemConfig = subconfig
+        currconfig[0].save()
+    return redirect('subsystemconfigs')
 
 def newcurrentconfig(request:WSGIRequest):
     if request.method == "POST":
